e115efc7e97804764c75896477d77903437c79e3
Your Name <you@example.com>
2018-04-16 02:12:15 +0900

    filter params

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module ExperimentalApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :ja
+    config.filter_parameters += %w[password password_confirmation uri]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
